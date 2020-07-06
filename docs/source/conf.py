87f111294710836675e15d5bd787abe09c0512ed
Aditya Mavalankar <adima0704@gmail.com>
2020-07-05 22:40:17 -0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-from recommonmark.transform import AutoStructify
-from recommonmark.parser import CommonMarkParser
 
 # -- Project information -----------------------------------------------------
 
@@ -39,7 +37,6 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinxcontrib.jsonschema', 'sphinx.ext.autosectionlabel', 'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
